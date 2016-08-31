18f770188c01adfe7cb6c2817e85d477afece62d
Jens Ackou <makkura@jenss-mbp-2.home>
2016-08-31 19:07:11 +0200

    relationship between pointers and arrays

---
+++ 42-array-passer/ArrayPasser.cpp
@@ -0,0 +1,44 @@
+/**
+ * ArrayPasser.cpp
+ * Contributors:
+ *  * Jens Ackou
+ * Description:
+ * demonstrates relationship between pointers and arrays
+ */
+
+#include <iostream>
+using namespace std;
+
+void increase(int* const array, const int NUM_ELEMENTS);
+void display(const int* const array, const int NUM_ELEMENTS);
+
+int main() {
+  cout << "Creating an array of high scores." << endl << endl;
+  const int NUM_SCORES = 3;
+  int highScores[NUM_SCORES] = {5000, 3500, 2700};
+
+  cout << "Displaying scores using array name as a constant pointer." << endl;
+  cout << *highScores << endl;
+  cout << *(highScores + 1) << endl;
+  cout << *(highScores + 2) << endl << endl;
+
+  cout << "Increasing scores by passing array as a constant pointer." << endl << endl;
+  increase(highScores, NUM_SCORES);
+
+  cout << "Displaying scores by passing array as a constant pointer to a constant." << endl;
+  display(highScores, NUM_SCORES);
+
+  return 0;
+}
+
+void increase(int* const array, const int NUM_ELEMENTS) {
+  for(int i = 0; i < NUM_ELEMENTS; ++i) {
+    array[i] += 500;
+  }
+}
+
+void display(const int* const array, const int NUM_ELEMENTS) {
+  for(int i = 0; i < NUM_ELEMENTS; ++i) {
+    cout << array[i] << endl;
+  }
+}
