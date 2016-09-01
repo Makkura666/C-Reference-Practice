60fc0b166384c46c49ce5712a6a6738324c0c4df
Jens Ackou <makkura@jenss-mbp-2.home>
2016-09-01 13:11:32 +0200

    Simulating node chains

---
+++ 52-game-lobby/GameLobby.cpp
@@ -0,0 +1,141 @@
+/**
+ * GameLobby.cpp
+ * Contributors:
+ *  * Jens Ackou
+ * Description:
+ * simulates a game lobby where players wait
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Player {
+public:
+  Player(const string& name = "");
+  string  GetName() const;
+  Player* GetNext() const;
+  void    SetNext(Player* next);
+
+private:
+  string  m_Name;
+  Player* m_pNext(Player* next);    // Pointer to next player in the list
+};
+
+Player::Player(const string& name = ""):
+  m_Name(name),
+  m_pNext(0)
+{}
+
+string Player::GetName() const {
+  return m_pName;
+}
+
+Player* Player::GetNext() const {
+  return m_pNext;
+}
+
+void Player:: SetNext(Player* next) {
+  m_pNext = next;
+}
+
+class Lobby {
+  friend ostream& operator<<(ostream& os, const Lobby& aLobby);
+
+public:
+  Lobby();
+  ~Lobby();
+  void AddPlayer();
+  void RemovePlayer();
+  void Clear();
+
+private:
+  Player* m_pHead;
+};
+
+Lobby::Lobby():
+  m_pHead(0)
+{}
+
+Lobby::~Lobby()
+{
+  Clear();
+}
+
+void Lobby::AddPlayer() {
+  // create a new player node
+  cout << "Please enter the name of the new player: ";
+  string name;
+  cin >> name;
+  Player* pNewPlayer = new Player(name);
+
+  // if list is empty, make head of list this new player
+  if (m_pHead == 0) {
+    m_pHead = pNewPlayer;
+  }
+  // otherwise find the end of the list and add the player there
+  else {
+    Player* pIter = m_pHead;
+    while (pIter->GetNext() != 0) {
+      pIter = pIter->GetNext();
+    }
+    pIter->SetNext(pNewPlayer);
+  }
+}
+
+void Lobby::RemovePlayer() {
+  if(m_pHead == 0) {
+    cout << "The game lobby is empty. No one to remove!" << endl;
+  } else {
+    Player* pTemp = m_pHead;
+    m_pHead = m_pHead->GetNext();
+    delete pTemp;
+  }
+}
+
+void Lobby::Clear() {
+  while (m_pHead != 0) {
+    RemovePlayer();
+  }
+}
+
+ostream& operator<<(ostream& os, const Lobby& aLobby) {
+  Player* pIter = aLobby.m_pHead;
+  os << endl << "Here's who's in the game lobby:" << endl;
+  if (pIter == 0) {
+    os << "The lobby is empty." << endl;
+  } else {
+    while (pIter != 0) {
+      os << pIter->GetName() << endl;
+      pIter = pIter->GetNext();
+    }
+  }
+
+  return os;
+}
+
+int main() {
+  Lobby myLobby;
+  int choice;
+
+  do {
+    cout << myLobby;
+    cout << endl << "GAME LOBBY" << endl;
+    cout << "0 - Exit the program." << endl;
+    cout << "1 - Add a player to the lobby." << endl;
+    cout << "2 - Remove a player from the lobby." << endl;
+    cout << "3 - Clear the lobby." << endl;
+    cout << endl << "Enter choice: ";
+    cin >> choice;
+
+    switch (choice) {
+      case 0: cout << "Good bye." << endl; break;
+      case 1: myLobby.AddPlayer(); break;
+      case 2: myLobby.RemovePlayer(); break;
+      case 3: myLobby.Clear(); break;
+      default: cout << "That was not a valid choice." << endl;
+    }
+  } while (choice != 0);
+
+  return 0;
+}
